828732010041f8d038366252e07b5369856029cf
arunkumr <m.arunkumar@live.in>
2022-05-20 01:05:37 +0530

    1 file modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { RoundProgressModule } from 'angular-svg-round-progressbar';
 import { DayProgressComponent } from './day-progress/day-progress.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, RoundProgressModule],
-  declarations: [AppComponent, HelloComponent, DayProgressComponent],
+  declarations: [AppComponent, DayProgressComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
